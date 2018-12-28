f7d1df33f1e6bd421c38e8665de76748583c44ea
Sonali Dasgupta <sonalidasgupta95.2011@gmail.com>
2018-12-28 22:58:54 +0530

    Changed the check for directory

--- dir_labels_restructure.py
+++ dir_labels_restructure.py
@@ -3,7 +3,7 @@ import shutil
 def create_dir_structure(dir = 'cifar/train'):
   fnames = [f for f in os.listdir(dir)]
   for fname in fnames:
-    if not os.path.isdir(dir):
+    if not os.path.isdir(f'{dir}/{fname}'):
       if not os.path.exists(f'{dir}/{fname.split("_")[1].split(".")[0]}'):
         os.mkdir(f'{dir}/{fname.split("_")[1].split(".")[0]}')
       shutil.move(f'{dir}/{fname}', f'{dir}/{fname.split("_")[1].split(".")[0]}/{fname}')
